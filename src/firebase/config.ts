8b39b2022d187f380d6e24b6183df49a4f859a37
Sinke Debalke <sinkedebalke390@gmail.com>
2025-12-24 17:06:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4817992643-78c5c",
+  "appId": "1:696394208776:web:db4f2a2177fb301987a3bd",
+  "apiKey": "AIzaSyCkZPYzWWo9BLyQTg4TsoSGlFYeuevkAm8",
+  "authDomain": "studio-4817992643-78c5c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "696394208776"
+};
