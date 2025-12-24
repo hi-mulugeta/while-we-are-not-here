547ccb242ac254455b1d7d3a0596f40718118ffc
Sinke Debalke <sinkedebalke390@gmail.com>
2025-12-24 18:42:41 +0000

    Error: ENOENT: no such file or directory, lstat '/vercel/path0/.next/ser

--- src/app/(app)/page.tsx
+++
@@ -1,9 +0,0 @@
-import MessageSlipForm from '@/components/message-slip-form';
-
-export default function AppPage() {
-  return (
-    <div className="p-4 sm:p-6 md:p-8">
-      <MessageSlipForm />
-    </div>
-  );
-}
